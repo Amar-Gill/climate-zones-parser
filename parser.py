62b7ec0c2a6c6d02afa2555baa0e3974d7f5a96a
Amardeep Gill <amardeepgill@BML7HClearadmins-MacBook-Pro.local>
2020-12-27 19:15:09 -0500

    first version of analyze features function

--- parser.py
+++ parser.py
@@ -6,10 +6,39 @@ import sys
 import json
 
 inFile = sys.argv[1]
-outFile = 'test_output.geojson'
+outFile = f'{sys.argv[1][0:-4]}.geojson'
+outFile2 = f'{sys.argv[1][0:-4]}-metadata.csv'
 
 climate_zones = ["Af", "Am", "As", "Aw", "BWk", "BWh", "BSk", "BSh", "Cfa", "Cfb", "Cfc", "Csa", "Csb", "Csc",
                  "Cwa", "Cwb", "Cwc", "Dfa", "Dfb", "Dfc", "Dfd", "Dsa", "Dsb", "Dsc", "Dsd", "Dwa", "Dwb", "Dwc", "Dwd", "EF", "ET"]
+
+main_climates = {
+    "A": "equatorial",
+    "B": "arid",
+    "C": "warm temperate",
+    "D": "snow",
+    "E": "polar"
+}
+
+precipitation = {
+    "W": "desert",
+    "S": "steppe",
+    "f": "fully humid",
+    "s": "summer dry",
+    "w": "winter dry",
+    "m": "monsoonal"
+}
+
+temperature = {
+    "h": "hot arid",
+    "k": "cold arid",
+    "a": "hot summer",
+    "b": "warm summer",
+    "c": "cool summer",
+    "d": "extremely continental",
+    "F": "polar frost",
+    "T": "polar tundra"
+}
 
 output_dict = {
     "type": "FeatureCollection",
@@ -23,8 +52,8 @@ output_dict = {
     "features": []
 }
 
-
-def parse(cz):
+# load dict
+def load_features(cz):
 
     feature = {
         "type": "Feature",
@@ -38,6 +67,7 @@ def parse(cz):
     }
 
     # begin parsing
+    # first pass - return sequence for polygon of a cz
     inf.seek(0)
 
     is_polygon = False
@@ -59,15 +89,40 @@ def parse(cz):
         if is_polygon:
             feature["geometry"]["coordinates"][polygon_index][0].append([
                                                                              line[1], line[0]])
-
     # end of parsing
+
 
     output_dict["features"].append(feature)
 
 
+# analyze_features
+# pass 2 - output length of multi-polygon nodes comrpising each cz
+# accept geojson feature dataformat
+def analyze_features(features):
+    for feature in features:
+        climate_zone = feature['properties']['climate_zone']
+        
+        polygon_count = len(feature['geometry']['coordinates'])
+
+        for polygon in feature['geometry']['coordinates']:
+            polygon_length = len(polygon[0])
+            print('climate_zone: ' + climate_zone + ' - polygon_nodes: ' + f'{polygon_length}' )
+
+        feature['properties']['polygon_count'] = polygon_count
+        feature['properties']['main_climate'] = main_climates[climate_zone[0]]
+
+        # if not climate_zone[0] == "E":
+        #     feature['properties']['precipitation'] = precipitation[climate_zone[1]]
+
+        # if not climate_zone[0] == "A":
+        #     feature['properties']['temperature'] = temperature[climate_zone[2]]
+
+# write geojson
 with open(inFile, 'r') as inf, open(outFile, 'w') as outf:
 
     for zone in climate_zones:
-        parse(zone)
+        load_features(zone)
+
+    analyze_features(output_dict['features'])
 
     json.dump(output_dict, outf)
